ac2aa5f1f74a75df6fc528fbc9bcc696c62032cb
Jose Paulo <joseropa@gmail.com>
2023-10-13 14:36:11 -0300

    User Delete

--- src/main/java/com/educandoweb/course/resouces/UserResource.java
+++ src/main/java/com/educandoweb/course/resouces/UserResource.java
@@ -1,19 +1,17 @@
 package com.educandoweb.course.resouces;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -43,8 +41,17 @@ public class UserResource {
 		user = userService.insert(user);
 		return ResponseEntity.status(HttpStatus.CREATED).body(user);
 
-		// URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		// URI uri =
+		// ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		// return ResponseEntity.created(uri).body(user);
+
+	}
+
+	@DeleteMapping(value = "{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 
 	}
 }
